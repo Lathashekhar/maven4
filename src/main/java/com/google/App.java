061f19d45edf8cb84f76c36816e2596b8fe52876
Lathashekhar <88106403+Lathashekhar@users.noreply.github.com>
2024-10-08 23:18:18 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -6,6 +6,7 @@ package com.google;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("welcome");
         
     }
 }
