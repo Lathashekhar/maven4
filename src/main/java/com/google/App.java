d093ebe99a28b7af04164d7d2c5c828c91254078
Lathashekhar <88106403+Lathashekhar@users.noreply.github.com>
2024-10-25 13:45:23 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("welcome");
+        System.out.printl("WElcome to Triggering");
         
     }
 }
